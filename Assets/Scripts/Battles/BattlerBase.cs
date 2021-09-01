09e28f9380b915f5fccd9538396f54ffbe88968a
wertrain <wertrain@gmail.com>
2021-09-02 01:08:44 +0900

    WIP

--- Assets/Scripts/Battles/BattlerBase.cs
+++ Assets/Scripts/Battles/BattlerBase.cs
@@ -7,5 +7,15 @@ using UnityEngine;
 /// </summary>
 public class BattlerBase : MonoBehaviour
 {
-    
+
+    public void UpdateBase()
+    {
+        if (_isActive)
+        {
+            var model = GameObject.Find(name + "/Geometry/Armature_Mesh");
+            model.GetComponent<Material>().color = new Color(1.0f, 1.0f, 1.0f, Mathf.Sin(Time.time) / 2 + 0.5f);
+        }
+    }
+
+    private bool _isActive;
 }
