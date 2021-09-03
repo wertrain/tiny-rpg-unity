8e13f96fb5e56a8f1a21085c55ea33eea2b7d338
wertrain <wertrain@gmail.com>
2021-09-03 23:29:13 +0900

    WIP

--- Assets/Scripts/BattleSceneManager.cs
+++ Assets/Scripts/BattleSceneManager.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Tsumugi.Text.Executing;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class BattleSceneManager : MonoBehaviour
 {
@@ -19,13 +22,30 @@ public class BattleSceneManager : MonoBehaviour
     /// <summary>
     /// 
     /// </summary>
+    private List<GameObject> _windows;
+
+    /// <summary>
+    /// 
+    /// </summary>
     private enum StateEventId : int
     {
         Enter,
         CommandSelect,
+        EnemySelect,
         Close,
         Max
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private enum WindowId : int
+    {
+        CommandSelect,
+        EnemySelect,
+        Max
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,19 +53,17 @@ public class BattleSceneManager : MonoBehaviour
         _stateMachine = new IceMilkTea.Core.ImtStateMachine<BattleSceneManager>(this);
         _stateMachine.AddAnyTransition<EnterState>((int)StateEventId.Enter);
         _stateMachine.AddAnyTransition<CommandSelectState>((int)StateEventId.CommandSelect);
+        _stateMachine.AddAnyTransition<EnemySelectState>((int)StateEventId.EnemySelect);
         _stateMachine.SetStartState<EnterState>();
 
         _battlers = new List<BattlerBase>();
 
-        var characters = GameObject.Find("Characters");
-        for (int index = 0; index < MyGameManager.Instance.PartyMembers.Length; ++index)
-        {
-            var member = MyGameManager.Instance.PartyMembers[index];
-            if (member == null) continue;
+        _windows = new List<GameObject>();
+        _windows.Add(GameObject.Find("CommandSelect"));
+        _windows.Add(GameObject.Find("EnemySelect"));
 
-            var prefab = CharacterDatabase.GetBattleCharacter(member.Id);
-            _battlers.Add(Instantiate(prefab, characters.transform).AddComponent<BattlerPlayer>());
-        }
+        _windows[(int)WindowId.CommandSelect].SetActive(false);
+        _windows[(int)WindowId.EnemySelect].SetActive(false);
     }
 
     // Update is called once per frame
@@ -84,12 +102,74 @@ public class BattleSceneManager : MonoBehaviour
     {
         protected internal override void Enter()
         {
+            var commandSelect = Context._windows[(int)WindowId.CommandSelect];
+            commandSelect.SetActive(true);
+            var position = commandSelect.transform.position;
+            var defaultX = position.x;
+            position.x = -400.0f;
+            commandSelect.transform.position = position;
 
+            LeanTween.moveX(commandSelect, defaultX, 0.5f).setOnComplete(() =>
+            {
+                var allButtons = new List<string>{ "AttackButton", "SkillButton", "DefenseButton" };
+                foreach(var buttonName in allButtons)
+                {
+                    var button = GameObject.Find(commandSelect.name + "/" + buttonName);
+                    button.GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        OnPress(button);
+                    });
+                }
+            });
         }
 
         protected internal override void Update()
         {
 
+        }
+
+        protected internal override void Exit()
+        {
+            var commandSelect = Context._windows[(int)WindowId.CommandSelect];
+
+            var allButtons = new List<string> { "AttackButton", "SkillButton", "DefenseButton" };
+            foreach (var buttonName in allButtons)
+            {
+                var button = GameObject.Find(commandSelect.name + "/" + buttonName);
+                button.GetComponent<Button>().onClick.RemoveAllListeners();
+            }
+
+            Context._windows[(int)WindowId.CommandSelect].SetActive(false);
+        }
+
+        private void OnPress(GameObject button)
+        {
+            switch (button.name)
+            {
+                case "AttackButton":
+                    Context._stateMachine.PushState();
+                    Context._stateMachine.SendEvent((int)StateEventId.EnemySelect);
+                    break;
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// EnemySelect ステート
+    /// </summary>
+    private class EnemySelectState : IceMilkTea.Core.ImtStateMachine<BattleSceneManager>.State
+    {
+        protected internal override void Enter()
+        {
+            var enemySelect = Context._windows[(int)WindowId.EnemySelect];
+            enemySelect.SetActive(true);
+        }
+
+        protected internal override void Exit()
+        {
+            var enemySelect = Context._windows[(int)WindowId.EnemySelect];
+            enemySelect.SetActive(false);
         }
     }
 }
