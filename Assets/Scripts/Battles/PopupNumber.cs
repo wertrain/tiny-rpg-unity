b0b346f1b5dcb31964a4b24851326f1c4a2d5c26
wertrain <wertrain@gmail.com>
2021-09-17 23:51:07 +0900

    WIP

--- Assets/Scripts/Battles/PopupNumber.cs
+++ Assets/Scripts/Battles/PopupNumber.cs
@@ -5,15 +5,26 @@ using UnityEngine.UI;
 
 public class PopupNumber : MonoBehaviour
 {
+    /// <summary>
+    /// 表示する番号
+    /// </summary>
     public int Number;
 
+    /// <summary>
+    /// テキストのフォント
+    /// </summary>
     public Font Font;
 
+    /// <summary>
+    /// フォントのサイズ
+    /// </summary>
     public int FontSize = 16;
 
     // Start is called before the first frame update
     void Start()
     {
+        _popupChars = new List<PopupChar>();
+
         var textNumber = Number.ToString();
         for (int index = 0; index < textNumber.Length; ++index)
         {
@@ -31,6 +42,8 @@ public class PopupNumber : MonoBehaviour
             var outline = container.AddComponent<Outline>();
             outline.effectDistance = new Vector2(1, -1);
             outline.effectColor = Color.black;
+
+            _popupChars.Add(new PopupChar(container));
         }
     }
 
@@ -39,4 +52,74 @@ public class PopupNumber : MonoBehaviour
     {
         
     }
+
+    private class PopupChar
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public GameObject GameObject { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public PopupChar(GameObject gameObject)
+        {
+            GameObject = gameObject;
+            _time = -1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Start()
+        {
+            _time = 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="deltaTime"></param>
+        public void Update(float deltaTime)
+        {
+            if (_time < 0) return;
+
+            _time += deltaTime;
+
+            if (_time < 0.5f)
+            {
+                _y += 1.0f;
+            }
+            else if (_time < 1.0f)
+            {
+                _y -= 1.0f;
+
+                if (_y <= 0.0f)
+                {
+                    _y = 0;
+                    _time = -1.0f;
+                }
+            }
+
+            var text = GameObject.GetComponent<Text>();
+            text.rectTransform.anchoredPosition = new Vector3(text.rectTransform.anchoredPosition.x, _y, 0);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private float _time;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private float _y;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private List<PopupChar> _popupChars;
 }
